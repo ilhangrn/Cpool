fee336a444f65fba46d3666de7dab72bc35cee52
ilhan <ilhangrn@hotmail.com>
2023-01-02 12:36:32 +0100

    barc example is added

---
+++ barc_demo/ig_test.c
@@ -0,0 +1,19 @@
+#define MAX     134 //  Not very important
+#define MINIMUM 23
+#define NONE    0 // Just for zero
+
+/*!
+ * @brief Gives the sum of two val.
+ *
+ * @param[in] num1 The first number to be sum
+ * @param[in] num2 The second number to be sum
+ *
+ * @return The value of the sum.
+ */
+int
+sum(int a; int b)
+{
+    int c = 0;
+    c     = a + b;
+    return c;
+}
