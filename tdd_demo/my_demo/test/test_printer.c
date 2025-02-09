dbe2556fdb3e1fa7075874385b46e52c2d2d17f0
ilhan goren <ilhangrn@hotmail.com>
2025-02-08 20:13:16 -0500

    ceedling module create is great

---
+++ tdd_demo/my_demo/test/test_printer.c
@@ -0,0 +1,21 @@
+
+#ifdef TEST
+
+#include "unity.h"
+
+#include "printer.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_printer_NeedToImplement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement printer");
+}
+
+#endif // TEST
